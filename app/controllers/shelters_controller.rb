ba0d3fa69d2660a50a71b465e70cec40180a2980
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-08 15:41:55 -0600

    Adds the ability to view a shelter's page and see data about the specific shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,5 +1,25 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ['shelter 1', 'shelter 2']
-  end 
+    @shelters = Shelter.all
+  end
+
+  def new
+  end
+
+  def create
+    shelter = Shelter.new({
+      id: params[:shelter][:id],
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+      shelter.save
+      redirects_to '/shelters'
+  end
+
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
 end
