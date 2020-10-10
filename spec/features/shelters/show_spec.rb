ca1b6b231dd44af61c71d0837b1edbbd7248ad73
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-09 23:50:45 -0600

    Adds test to verify visiting specific shelters

---
+++ spec/features/shelters/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+  describe "As a visitor" do
+    describe "When I visit '/shelters/:id'" do
+      it "Then I see the shelter with that ID including the shelter's
+      name, address, city, state, and zip" do
+        shelter_1 = Shelter.create({name: 'DFL',
+                                    address: '123 lane',
+                                    city: 'Oakland',
+                                    state: 'CA',
+                                    zip: '123456'
+                                    })
+
+        visit "/shelters/#{shelter_1.id}"
+        save_and_open_page
+
+        expect(page).to have_content(shelter_1.name)
+        expect(page).to have_content(shelter_1.address)
+        expect(page).to have_content(shelter_1.city)
+        expect(page).to have_content(shelter_1.state)
+        expect(page).to have_content(shelter_1.zip)
+      end
+    end
+  end
